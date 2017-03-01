d2b0c4c0805da4d1f54051a5ea046709bf4249c3
Abou Fofana <Abou Fofana>
2017-03-01 12:46:03 +0000

    Added files and changes required for deployment to heroku

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'locallibrarydata',
+        'USER':'postgres',
+        'PASSWORD': 'assata',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
